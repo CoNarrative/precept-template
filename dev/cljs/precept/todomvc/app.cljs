3508ce5532d505e984d7dcf8fd1c65d4501a99dc
Adam Frey <adam@adamfrey.me>
2017-06-06 21:04:55 -0400

    Make template friendlier for code reloading
    Before the change to make the history object a defonce, I was getting this error
    in the console:
    
    ```
    Failed to execute 'write' on 'Document': It isn't possible to write into a
    document from an asynchronously-loaded external script unless it is explicitly
    opened.
    ```
    
    I also fixed the on-jsload function to reload updated versions of the var.

--- dev/cljs/precept/todomvc/app.cljs
+++ dev/cljs/precept/todomvc/app.cljs
@@ -3,6 +3,6 @@
             [figwheel.client :as figwheel :include-macros true]))
 
 (figwheel/watch-and-reload
-  :on-jsload core/mount-components)
+  :on-jsload #'core/mount-components)
 
 (core/main)
