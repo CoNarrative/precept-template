d6840eabc44d0ab2d9e483f46a972077f1a18d3d
Alex Dixon <alex@conarrative.com>
2017-06-07 07:32:24 -0700

    0.0.3

--- precept/project.clj
+++ precept/project.clj
@@ -1,4 +1,4 @@
-(defproject precept/lein-template "0.0.1"
+(defproject precept/lein-template "0.0.3"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
